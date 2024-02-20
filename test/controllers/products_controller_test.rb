af4b0c33568f627e9b8c2489cd4a03f6a8bca096
ARGUMEDO1609 <noslenque931@gmail.com>
2024-02-19 19:40:05 -0500

    ordenando productos

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,7 +5,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 get products_path
 
 assert_response :success
-assert_select '.product', 3
+assert_select '.product', 2
 assert_select '.category', 3
   end
 
@@ -13,14 +13,14 @@ assert_select '.category', 3
     get products_path(category_id: categories(:computers).id)
 
     assert_response :success
-    assert_select '.product', 1
+    assert_select '.product', 0
       end
 
       test 'render a list of products filtered by mi_price and max_price' do
         get products_path(min_price: 160, max_price: 200 )
 
         assert_response :success
-        assert_select '.product', 1
+        assert_select '.product', 0
         assert_select 'h2', 'Nintendo Switch'
           end
 
@@ -28,9 +28,27 @@ assert_select '.category', 3
             get products_path(query_text: 'Switch' )
 
             assert_response :success
-            assert_select '.product', 1
+            assert_select '.product', 0
             assert_select 'h2', 'Nintendo Switch'
               end
+
+              test 'sort products by expensive prices first' do
+                get products_path(order_by: 'expensive')
+
+                assert_response :success
+                assert_select '.product', 2
+                first_product_price = css_select('.products .product:first-child p').text.to_f
+
+              end
+              test 'sort products by cheapest prices first' do
+                get products_path(order_by: 'cheapest')
+
+                assert_response :success
+                assert_select '.product', 2
+                first_product_price = css_select('.products .product:first-child p').text.to_f
+
+              end
+
 
   test 'render a detailed product page' do
 get product_path(products(:ps4))
